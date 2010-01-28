1e9e9c101bb8acd674f66d3b119470b5a8628944
Jakub Kosinski <jakub.kosinski@ghandal.net>
2010-01-28 19:07:59 +0100

    added plugin installation file

---
+++ install.rb
@@ -0,0 +1,4 @@
+puts ""
+puts "Make sure to add Cookie domains to your middleware stack in your application:"
+puts '  config.middleware.use("CookieDomain", ".example.com", ".example.net", ".example.org")'
+puts ""
