abf6995cc8a64a7b5b3806fd752ce6f8dcc79fd0
Jakub Kosinski <jakub.kosinski@ghandal.net>
2010-01-28 19:09:38 +0100

    fixed typo in install.rb

--- install.rb
+++ install.rb
@@ -1,4 +1,4 @@
 puts ""
 puts "Make sure to add Cookie domains to your middleware stack in your application:"
-puts '  config.middleware.use("CookieDomain", ".example.com", ".example.net", ".example.org")'
+puts '  config.middleware.use("CookieDomains", ".example.com", ".example.net", ".example.org")'
 puts ""
